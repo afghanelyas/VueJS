fc25b564d00612890b4182233194fb143f2c1eb0
afghanelyas <afghan.elyas1@gmail.com>
2023-08-05 22:56:09 +0430

    create component

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,12 +1,32 @@
 export default {
     template: `
-        <button class="bg-blue-500 text-white px-4 py-2 rounded disabled:cursor-not-allowed" :disabled="proccessing">
+        <button
+            :class="{
+                'border bg-red-900 rounded px-5 py-2 disabled:cursor-not-allowed': true,
+                'bg-blue-200 hover:bg-blue-400': type === 'primary',
+                'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+                'bg-gray-200 hover:bg-gray-400': type === 'muted',
+                'is-loading': processing
+            }"
+            :disabled = "processing"
+            >
             <slot />
         </button>
         `,
+        props:{
+            type: {
+                type: String,
+                default: 'primay'
+            },
+            processing:{
+                type: Boolean,
+                default: false
+            }
+
+        },
             data() {
                 return {
-                            proccessing: true
+                            processing: true
                         }
              },
 }
